f003489324ec08deef19094138132218c5edd995
Bhavya191007 <202501144@dau.ac.in>
2025-10-08 23:28:27 +0530

    main.c
    updated version

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//#include <sys/select.h>  // For select() - POSIX
-#include <winsock2.h>
-#include <ws2tcpip.h>
+#include <sys/select.h>  // For select() - POSIX
 #include <sys/time.h>
 #include <unistd.h>      // For read()
 #include <signal.h>      // For signal handling (backup)
